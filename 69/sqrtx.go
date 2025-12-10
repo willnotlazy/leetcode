d230fb89f41d87f95a3cc0f6c10a5abc86526c4f
willnotlazy <519478450@qq.com>
2025-12-10 13:29:11 +0800

    init

---
+++ 69/sqrtx.go
@@ -0,0 +1,46 @@
+package sqrtx
+
+/**
+69. x 的平方根
+给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+
+示例 1：
+输入：x = 4
+输出：2
+
+示例 2：
+输入：x = 8
+输出：2
+解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+
+提示：
+0 <= x <= 2^31 - 1
+**/
+
+func mySqrt(x int) int {
+	if x == 0 {
+		return 0
+	}
+
+	if x < 4 {
+		return 1
+	}
+
+	left, right := 0, x/2
+	ans := 0
+	for left <= right {
+		mid := left + (right-left)/2
+		if mid*mid > x {
+			right = mid - 1
+		} else if mid*mid <= x {
+			left = mid + 1
+			if ans <= mid {
+				ans = mid
+			}
+		}
+	}
+
+	return ans
+}
